f9f39be5fb7ea082a29a5af312d380a9cb7464ef
soy <soysauce1223@naver.com>
2024-11-07 19:52:18 +0900

    [FEAT] Record Manager  기능 추가

--- Sparta3_BaseballGame_Logic/BaseballGame.swift
+++ Sparta3_BaseballGame_Logic/BaseballGame.swift
@@ -13,48 +13,52 @@ class BaseballGame {
     var input : [Int] = []
     var numArr = [0,1,2,3,4,5,6,7,8,9]
     var randomNums : [Int] = [1,2,3]
-    var recordArr : [Int] = []
     
     var strike = 0
     var ball = 0
-    var record = 0
     
+    var record = 0
+    let recordManager = RecordManager()
     
     func start() {
-        print("환영합니다! 원하는 번호를 입력해주세요.")
-        print("1. 게임 시작하기  2. 게임 기록 보기  3. 종료하기")
-        
-        var insertNum = Int(readLine()!)!
-        
-        switch insertNum {
-        case 1:
-            game()
-        case 2:
-            let game = BaseballGame()
-            game.start()
-        case 3:
-            let game = BaseballGame()
-            game.start()
-        default:
-            let game = BaseballGame()
-            game.start()
+        while true {
+            print("환영합니다! 원하는 번호를 입력해주세요.")
+            print("1. 게임 시작하기  2. 게임 기록 보기  3. 종료하기")
+            
+            if let input = readLine(), let insertNum = Int(input) {
+                switch insertNum {
+                case 1:
+                    gameMode()
+                    recordManager.add(record: record)
+                case 2:
+                    recordManager.showRecords()
+                case 3:
+                    print("< 숫자 야구 게임을 종료합니다. >")
+                    return  // 게임 종료 후 함수 종료
+                default:
+                    print("올바르지 않은 입력입니다. 1, 2, 3 중에서 선택해주세요.")
+                }
+            } else {
+                print("숫자를 입력해주세요.")
+            }
         }
     }
     
     func makeAnswer() {
         // 정답 생성
-        /*for i in 0...2 {
-         if let num = numArr.randomElement() {
-         randomNums.append(num)
-         numArr.remove(at: numArr.firstIndex(of: num)!)
-         } else {
-         print("nil")
-         }
-         }*/
+        for _ in 0...2 {
+            if let num = numArr.randomElement() {
+                randomNums.append(num)
+                numArr.remove(at: numArr.firstIndex(of: num)!)
+            }
+        }
     }
     
-    func game() {
-        makeAnswer()
+    func gameMode() {
+        //makeAnswer()
+        
+        strike = 0
+        ball = 0
         
         while true {
             record = record + 1
@@ -71,23 +75,26 @@ class BaseballGame {
                 }
             }
             print(input)
+
+            strike = 0
+            ball = 0
             
-            for i in input {
+            for i in 0..<input.count {
                 //스트라이크 계산
-                if i == randomNums[i] {
-                    strike = strike + 1
+                if input[i] == randomNums[i] {
+                    strike += 1
                 }
                 
                 //볼 계산
-                if randomNums.contains(i) == true {
-                    ball = ball + 1
+                if randomNums.contains(input[i]) == true {
+                    ball += 1
                 }
             }
-            ball = ball - strike // 볼 카운트에서 스트라이크 값 제외
+            ball -= strike // 볼 카운트에서 스트라이크 값 제외
             
             if strike == 3 {
                 print("정답입니다!")
-                break
+                return
             } else if strike == 0 && ball == 0 {
                 print("Nothing")
             }
@@ -100,6 +107,5 @@ class BaseballGame {
             
             print("\(strike) Strike \(ball) Ball")
         }
-        recordArr.append(record)
     }
 }
--- Sparta3_BaseballGame_Logic/RecordManager.swift
+++ Sparta3_BaseballGame_Logic/RecordManager.swift
@@ -5,8 +5,24 @@
 //  Created by seohuibaek on 11/7/24.
 //
 
-import Foundation
-
 class RecordManager {
+    var records: [Int] = [] // 게임 기록을 저장할 배열
     
+    // 게임 기록 추가
+    func add(record: Int) {
+        records.append(record)
+    }
+    
+    // 저장된 게임 기록 출력
+    func showRecords() {
+        if records.isEmpty {
+            print("저장된 게임 기록이 없습니다.")
+        } else {
+            print("게임 기록:")
+            for (index, record) in records.enumerated() {
+                print("게임 \(index + 1): \(record)회")
+            }
+        }
+    }
 }
+
